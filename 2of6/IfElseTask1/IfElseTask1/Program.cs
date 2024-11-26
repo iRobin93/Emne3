63768d35dd83c24c1f3421689732ee82fb4358d0
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-11-26 14:31:07 +0100

    commits

--- 2of6/IfElseTask1/IfElseTask1/Program.cs
+++ 2of6/IfElseTask1/IfElseTask1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Number1 = 1;
+int Number2 = 2;
+int Number3 = 1;
+
+bool CheckNumbers = checkNumbers(Number1, Number2);
+bool CheckNumbers2 = checkNumbers(Number1, Number3);
+
+bool checkNumbers(int Number1, int Number2)
+{
+    if (Number1 == Number2)
+    { return true; }
+    else return false;
+}
+
+Console.WriteLine("Ulike Numre: " + CheckNumbers);
+Console.WriteLine("Like Numre: " + CheckNumbers2);
+
+Console.WriteLine("");
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
+
---
+++ 2of6/TwoNumbersSumOrMultiplied/TwoNumbersSumOrMultiplied/Program.cs
@@ -0,0 +1,22 @@
+﻿int Number1 = 5;
+int Number2 = 6;
+int Number3 = 5;
+
+int Number4 = SumOrMultiply(Number1, Number2);
+int Number5 = SumOrMultiply(Number1, Number3);
+
+
+int SumOrMultiply(int Number1, int Number2)
+{
+    if (Number1 == Number2)
+    { return Number1 * Number2; }
+    else return Number1 + Number2;
+
+}
+
+Console.WriteLine(" 5 og 6 er: " + Number4);
+Console.WriteLine("5 og 5 er: " + Number5);
+
+Console.WriteLine("");
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
