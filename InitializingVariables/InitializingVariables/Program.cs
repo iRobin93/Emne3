b08d6c45dba4824087297cba00189d7d6a3c75e6
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-11-26 13:25:31 +0100

    Add project files.

---
+++ InitializingVariables/InitializingVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿int Number1 = 5;
+long Long1 = 6;
+float Float1 = 7.6F;
+decimal Decimal1 = 1.1M;
+double Double1 = 2.2;
+string String1 = "Hei";
+char Char1  = 'A';
+bool Bool1 = true;
+
+Console.WriteLine($"Nr 1: {Number1}");
+Console.WriteLine($"Long1: {Long1}");
+Console.WriteLine($"Float1: {Float1}");
+Console.WriteLine($"Decimal1: {Decimal1}");
+Console.WriteLine($"Double1: {Double1}");
+Console.WriteLine($"String1: {String1}");
+Console.WriteLine($"Char1: {Char1}");
+Console.WriteLine($"Bool1: {Bool1}");
