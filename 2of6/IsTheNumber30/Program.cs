91b53cbe5ced2ac49ab3f4a9ad28efc11923daf2
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-11-27 10:00:32 +0100

    commits

---
+++ 2of6/IsTheNumber30/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int Number1 = 15;
+int Number2 = 16;
+int Number3 = 15;
+int Number4 = 30;
+
+bool Bool1 = TakeTwoNumbers(Number1, Number2);
+bool Bool2 = TakeTwoNumbers(Number1, Number3);
+bool Bool3 = TakeTwoNumbers(Number1, Number4);
+bool TakeTwoNumbers(int Number1, int Number2)
+{
+
+    if (Number1 + Number2 == 30 || Number1 == 30 || Number2 == 30)
+    {
+        return true;
+    }
+    else return false;
+
+}
+
+
+Console.WriteLine("15 and 16: " + Bool1);
+Console.WriteLine("15 and 15: " + Bool2);
+Console.WriteLine("15 and 30: " + Bool3);
+Console.WriteLine("");
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
---
+++ StartProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+namespace StartProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("ingen parametre");
+                }
+                else
+                {
+                    string myProgram = args[0];
+                    myProgram = myProgram.Substring(13);
+                    Process.Start(Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + "\\source\\repos\\" + myProgram);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
