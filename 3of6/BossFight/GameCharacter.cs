cafc7c448f028e3e0344e2b62c7e06b530273d0d
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-12-04 15:07:33 +0100

    x

---
+++ 3of6/BossFight/GameCharacter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossFight
+{
+    internal class GameCharacter
+    {
+        private string _name;
+        private int _health;
+        private int _strength;
+        private int _stamina;
+        private int _startingStamina;
+
+        public GameCharacter(string name, int health, int strength, int stamina)
+        {
+            _name = name;
+            _health = health;
+            _strength = strength;
+            _stamina = stamina;
+            _startingStamina = stamina;
+        }
+        public void Recharge()
+        {
+            _stamina = _startingStamina;
+            Console.WriteLine(_name+ " had to recharge he has regained " + _startingStamina);
+            Console.WriteLine("Trykk en tast for å gå tilbake til options");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+        public bool Fight(GameCharacter opponent)
+        {
+
+
+            Console.Clear();
+            if(_stamina == 0)
+            {
+                Recharge();
+                return false;
+            }
+            if(_name == "Boss")
+            {
+                Random random = new Random();
+                _strength = random.Next(0, 31);
+            }
+                 
+            opponent._health -= _strength;
+            _stamina -= 10;
+
+            if (opponent._health <= 0)
+            {
+                Console.WriteLine("We have a winner! Winner is " + _name);
+                Console.ReadKey();
+                return true;
+                
+            }
+
+            Console.WriteLine(_name + " Has hit " + opponent._name + " for " + _strength + "damage!");
+            Console.WriteLine(opponent._name + " has " + opponent._health + " health left.");
+            Console.WriteLine("Trykk en tast for å gå tilbake til options");
+            Console.ReadKey();
+            Console.Clear();
+            return false;
+        }
+
+    }
+}
--- 3of6/BossFight/Program.cs
+++ 3of6/BossFight/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using BossFight;
+
+GameCharacter Hero = new GameCharacter("Hero", 100, 20, 40);
+GameCharacter Boss = new GameCharacter("Boss", 400, 20, 10);
+bool BossTurn = true;
+bool end;
+
+
+
+
+while (true)
+{
+    Console.WriteLine("Velkommen til BossFight, trykk 1 for å fighte, 5 for å avslutte");
+
+var input = Console.ReadKey();
+    if (input.KeyChar == '5')
+    {
+        goto trueEnd;
+    }
+
+    if (BossTurn)
+    {
+        end = Boss.Fight(Hero);
+        BossTurn = false;
+    }
+
+    else
+    {
+        end = Hero.Fight(Boss);
+        BossTurn = true;
+    }
+
+    if (end)
+        goto end;
+
+}
+
+
+
+
+end:
+
+while (true)
+{
+    
+    Console.WriteLine("Tast 5 for å avslutte");
+    var input1 = Console.ReadKey();
+    if (input1.KeyChar == '5')
+        goto trueEnd;
+}
+trueEnd:;
+
+
+
+
