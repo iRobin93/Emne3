4fa60ad4d5c2d7258091b153a5dfff773f3e83b7
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-12-04 11:29:58 +0100

    x

---
+++ 3of6/FriendFace/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Oppgave FriendFace:
+ * 1. Lage nødvendige klasser: user
+ * 2. lage metodene AddFriend og RemoveFriend
+ * 3. Lage meny
+ * 4. lage brukere.
+ * 
+ * 
+ */
+
+
+using System.Reflection.Metadata;
+using FriendFace;
+ConsoleKeyInfo input;
+User MyUser = new User("Robin", "Test1@hotmail.no", "1234", "Hei, jeg heter Robin");
+User.Users.Add(MyUser);
+User.Users.Add(new User("Ole Morten", "Test2@hotmail.no", "1234", "Hei, jeg heter Ole Morten"));
+User.Users.Add(new User("Mona", "Test3@hotmail.no", "1234", "Hei, jeg heter Mona"));
+User.Users.Add(new User("Charlotte", "Test4@hotmail.no", "1234", "Hei, jeg heter Charlotte1"));
+User.Users.Add(new User("Charlotte", "Test5@hotmail.no", "1234", "Hei, jeg heter Charlotte2"));
+
+
+while (true)
+{
+    
+    WriteMenu();
+     input = Console.ReadKey();
+
+
+    switch (input.KeyChar)
+    {
+        case '1':
+            MyUser.AddFriend();
+            break;
+        case '2':
+            MyUser.RemoveFriend();
+            break;
+        case '3':
+            MyUser.ShowFriends();
+            break;
+        case '4':
+            MyUser.WriteFriendInfo();
+            break;
+        case '5': goto EndWhile;
+            break;
+    }
+    Console.Clear();
+}
+EndWhile:
+
+
+
+
+void WriteMenu()
+{
+    Console.WriteLine("Hei og velkommen til FriendFace");
+    Console.WriteLine("Tast 1 for å legge til en venn");
+    Console.WriteLine("Tast 2 for å fjerne en venn");
+    Console.WriteLine("Tast 3 for å vise alle dine venner");
+    Console.WriteLine("Tast 4 for å velge en venn og å printe ut infoen deres");
+    Console.WriteLine("Tast 5 for å avslutte");
+
+}
---
+++ 3of6/FriendFace/User.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendFace
+{
+    internal class User
+    {
+        public static List<User> Users = new List<User>();
+        public static int HighestId = 0;
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+
+        public string Password { get; private set; }
+
+        public int Id { get; private set; }
+        public string Info { get; private set; }
+        public List<User> friends { get; private set; } = new List<User>();
+        
+
+        
+
+        public User(string name, string email, string password, string info) 
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            Id = GetId();
+            Info = info;
+        }
+        public void AddFriend()
+        {
+            
+            Console.Clear();
+            foreach (var user in Users)
+            {
+                if (user.Id != 1 && !friends.Contains(user))
+                Console.WriteLine("Tast " + user.Id + " for å legge til " + user.Name);
+                
+            }
+
+            var input = Console.ReadKey();
+            foreach (var user in Users) 
+            {
+                if(input.KeyChar.ToString() == user.Id.ToString())
+                    if (user.Id != 1 && !friends.Contains(user))
+                        friends.Add(user);
+            }
+            
+        }
+
+        public void RemoveFriend()
+        {
+            Console.Clear();
+            foreach (var friend in friends)
+                Console.WriteLine(friend.Name + " ID: " + friend.Id);
+            Console.WriteLine("Trykk en ID for å slette venn");
+            var input = Console.ReadKey();
+            foreach (var friend in friends)
+            {
+                if (friend.Id.ToString() == input.KeyChar.ToString())
+                {
+                    friends.Remove(friend);
+                    return;
+                }
+
+            }
+        }
+
+        public void ShowFriends()
+        {
+            Console.Clear();
+            foreach (var friend in friends)
+                Console.WriteLine(friend.Name + " ID: " + friend.Id);
+            Console.WriteLine("Trykk en tast for å gå tilbake til meny");
+            Console.ReadKey();
+        }
+
+        public void WriteFriendInfo()
+        {
+
+        }
+
+        public int GetId()
+        {
+            HighestId++;
+            return HighestId;
+        }
+    }
+}
