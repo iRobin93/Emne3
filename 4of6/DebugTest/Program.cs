57b043cca4a8a15748912f4caa795f7638825c24
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-12-05 11:21:44 +0100

    x

---
+++ 4of6/DebugTest/Program.cs
@@ -0,0 +1,32 @@
+﻿var range = 250;
+
+var counts = new int[range];
+string text = "something";
+while (!string.IsNullOrWhiteSpace(text))
+{
+    text = Console.ReadLine();
+    foreach (var character in text ?? string.Empty)
+    {
+        counts[(int)character]++;
+    }
+    for (var i = 0; i < range; i++)
+    {
+        if (counts[i] > 0)
+        {
+            var character = (char)i;
+            Console.WriteLine(character + " - " + counts[i]);
+        }
+    }
+}
+
+
+
+
+
+//Breakpoints : 1, 8, 12, 20.
+//Verdi 72, H
+/*
+ * 
+ * 
+ * 
+ */
